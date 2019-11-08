657a21af5b0fda6b62be24d171802e62ff0313c6
qmnguyen4 <43012292+qmnguyen4@users.noreply.github.com>
2019-11-07 22:08:23 -0800

    Add files via upload

---
+++ BinaryHeap.java
@@ -0,0 +1,152 @@
+import java.util.Arrays;
+
+public class BinaryHeap{
+    private static final int DEFAULT_CAPACITY = 10;
+    protected T[] array;
+    protected int size;
+    
+
+	public BinaryHeap () {
+
+        array = (T[])new Comparable[DEFAULT_CAPACITY];  
+        size = 0;
+    }
+    
+    
+  
+    public void add(T value) {
+
+        if (size >= array.length - 1) {
+            array = this.resize();
+        }        
+
+        size++;
+        int index = size;
+        array[index] = value;
+        
+        bubbleUp();
+    }
+    
+ 
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    
+ 
+    public T peek() {
+        if (this.isEmpty()) {
+            throw new IllegalStateException();
+        }
+        
+        return array[1];
+    }
+
+    
+  
+    public T remove() {
+    	// what do want return?
+    	T result = peek();
+    	
+    	// get rid of the last leaf/decrement
+    	array[1] = array[size];
+    	array[size] = null;
+    	size--;
+    	
+    	bubbleDown();
+    	
+    	return result;
+    }
+    
+  
+    public String toString() {
+        return Arrays.toString(array);
+    }
+
+   
+    protected void bubbleDown() {
+        int index = 1;
+        
+        // bubble down
+        while (hasLeftChild(index)) {
+            // which of my children is smaller?
+            int smallerChild = leftIndex(index);
+            
+            // bubble with the smaller child, if I have a smaller child
+            if (hasRightChild(index)
+                && array[leftIndex(index)].compareTo(array[rightIndex(index)]) > 0) {
+                smallerChild = rightIndex(index);
+            } 
+            
+            if (array[index].compareTo(array[smallerChild]) > 0) {
+                swap(index, smallerChild);
+            } else {
+                // otherwise, get outta here!
+                break;
+            }
+            
+            // make sure to update loop counter/index of where last el is put
+            index = smallerChild;
+        }        
+    }
+    
+    
+    
+    protected void bubbleUp() {
+        int index = this.size;
+        
+        while (hasParent(index)
+                && (parent(index).compareTo(array[index]) > 0)) {
+            // parent/child are out of order; swap them
+            swap(index, parentIndex(index));
+            index = parentIndex(index);
+        }        
+    }
+    
+    
+    protected boolean hasParent(int i) {
+        return i > 1;
+    }
+    
+    
+    protected int leftIndex(int i) {
+        return i * 2;
+    }
+    
+    
+    protected int rightIndex(int i) {
+        return i * 2 + 1;
+    }
+    
+    
+    protected boolean hasLeftChild(int i) {
+        return leftIndex(i) <= size;
+    }
+    
+    
+    protected boolean hasRightChild(int i) {
+        return rightIndex(i) <= size;
+    }
+    
+    
+    protected T parent(int i) {
+        return array[parentIndex(i)];
+    }
+    
+    
+    protected int parentIndex(int i) {
+        return i / 2;
+    }
+    
+    
+    protected T[] resize() {
+        return Arrays.copyOf(array, array.length * 2);
+    }
+    
+    
+    protected void swap(int index1, int index2) {
+        T tmp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = tmp;        
+    }
+}
